8a2815ad5de3a01783c2d60e91f0d3a4aca1f57b
Peihai Luo （罗培海） <phluo@trip.com>
2023-09-10 09:42:10 +0800

    feat: add basename

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <Router basename={"/github-page-demo"}>
       <Switch>
         <Route path="/detail" component={Detail} />
         <Route path="/" component={Home} />
